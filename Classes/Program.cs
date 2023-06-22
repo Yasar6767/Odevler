e6fce29d249c8b8643723afbc5fc931ced85cb3f
Yaşar Mehmet Akat <akat434@gmail.com>
2023-06-22 16:06:22 +0300

    Add project files.

---
+++ Classes/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Classes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomerManager customerManager = new CustomerManager();
+            customerManager.Add();
+            customerManager.Update();
+
+            ProductManager productManager = new ProductManager();
+            productManager.Add();
+            productManager.Update();
+
+            Customer customer1 = new Customer();
+            customer1.Id = 1;
+            customer1.City = "Zonguldak";
+            customer1.FirstName = "Yaşar";
+            customer1.LastName = "Akat";
+
+            Customer customer2 = new Customer
+            {
+                Id = 2, City = "Trabzon", FirstName = "Elif", LastName = "Akat"
+            };
+
+            Console.WriteLine(customer2.FirstName);
+
+            Console.ReadLine();
+        }
+    }
+}
